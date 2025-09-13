be11f34583175a6de43096211395cad0fdb353e5
osamamohamed911 <osama.mohammed@salla.sa>
2025-09-13 13:06:09 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+ content: ["./**/*.html", "./style/**/*.{css,js}"], 
+  theme: {
+    extend: {
+      fontFamily: {
+        cairo: ['Cairo', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
